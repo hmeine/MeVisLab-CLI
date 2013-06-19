de8f16ed744783b8b21ef83eb99548327e967605
Hans Meine <hans_meine@gmx.net>
2013-06-19 12:58:18 -0400

    add clear() method for when autoUpdate is off

--- CLIModuleBackend.py
+++ CLIModuleBackend.py
@@ -1,11 +1,21 @@
 def updateIfAutoApply():
     if ctx.field("autoApply").value:
         update()
+    else:
+        clear()
 
 def updateIfAutoUpdate():
     if ctx.field("autoUpdate").value:
         update()
+    else:
+        clear()
 
 def update():
+    """Execute the CLI module"""
     print "<fill in CLI module execution here>"
+
+def clear():
+    """Close all itkImageFileReaders such as to make the output image states invalid"""
+    for o in ctx.outputs():
+        ctx.module(o).field("close").touch()
 
